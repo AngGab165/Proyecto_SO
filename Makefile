2646a65adf37832c745288413ad11514ee6d254e
Angel Gabo <cbi2223046760@izt.uam.mx>
2026-03-27 02:26:57 +0000

    Cambios implementados del laboratorio del viernes

--- Makefile
+++ Makefile
@@ -1,6 +1,30 @@
-all : leeMBR
-leeMBR: leeMBR.o leeGPT.o leeAPFS.o
-	gcc -g -o leeMBR leeMBR.o leeGPT.o leeAPFS.o -l curses
+BIN = leeMBR
+
+all: $(BIN)
+
+SOURCE = \
+		leeMBR.c \
+		leeGPT.c \
+		leeAPFS.c \
+		checksum.c \
+
+CC = gcc
+CC_FLAGS = -g -Wall
+LIB_FLAGS = -l curses
+
+OBJ = $(SOURCE:%.c=%.o)
+
+$(BIN) : $(OBJ)
+		echo Ligando...
+		#Solo liga
+		$(CC) $(OBJ) $(LIB_FLAGS) -o $(BIN)
 
 %.o : %.c
-	gcc -g -c $<
+		#Compila mismo nombre en .o
+		$(CC) $(CC_FLAGS) -c $<
+
+.PHONY: clean
+clean:
+		echo Limpiando...
+		~rm -r $(OBJ)
+		~rm -r $(BIN)
--- leeAPFS.c
+++ leeAPFS.c
@@ -1,8 +1,8 @@
-#include "gpt.h"
 #include <string.h>
 #include <ncurses.h>
-
+#include "gpt.h"
 #include "APFS.h"
+#include "checksum.h"
 
 nx_superblock_t sb;
 
@@ -17,6 +17,68 @@ int leeAPFS(char *base){
 
     mvprintw(10, 5, "Firma: %s Tamaño: %ld", firma, sb.nx_block_count*sb.nx_block_size);
 
+    if(sb.nx_magic != NX_MAGIC){
+        mvprintw(12, 5, "No es un sistema de archivos APFS");
+        return -1;
+    }
+
+    bool ck = is_cksum_valid((uint32_t *) base, sb.nx_block_size);
+
+    if(ck == false){
+        return -1;
+    }
+
+    //Valida y obten la transaccion mas alta(reciente)
+    uint64_t ini =sb.nx_xp_desc_base * sb.nx_block_size;
+    uint64_t ultima_xid = 0;
+    uint64_t idx_ultima_xid = 0;
+
+    for(uint32_t i=0; i < sb.nx_xp_desc_blocks; i++){
+        //Valida los bloques 
+        char *bl = base + ini + i*sb.nx_block_size;
+        bool ck = is_cksum_valid((uint32_t *) bl, sb.nx_block_size);
+        if(ck == false){
+            return -1;
+        }
+
+        obj_phys_t enc;
+        memcpy(&enc, bl, sizeof(enc));
+        if(enc.o_type & OBJECT_TYPE_NX_SUPERBLOCK){
+            if(ultima_xid < enc.o_xid){
+                ultima_xid = enc.o_xid;
+                idx_ultima_xid = i;
+            }
+        }
+    }
+
+    //El superbloque que queremos esta en idx_ultima_xid
+    char *nbs = base + (sb.nx_xp_desc_base + idx_ultima_xid) * sb.nx_block_size;
+    memcpy(&sb, nbs, sizeof(sb));
+
+    char *omap = base + sb.nx_omap_oid * sb.nx_block_size;
+    if(is_cksum_valid((uint32_t *)omap, sb.nx_block_size) == false){
+        return -1;
+    }
+
+    omap_phys_t mapa;
+    memcpy(&mapa, omap, sizeof(mapa));
+
+    btree_node_phys_t arbb;
+    char *bt = base + mapa.om_tree_oid * sb.nx_block_size;
+    if(is_cksum_valid((uint32_t *)bt, sb.nx_block_size) == false){
+        return -1;
+    }
+
+    memcpy(&arbb, bt, sizeof(arbb));
+
+    int fs = sb.nx_fs_oid[0];
+
+    char *ns = base + (75)*sb.nx_block_size;
+    memcpy(&sb, ns, sizeof(sb));
+
+
+
+    refresh();
     getch();
     return 0;
 
